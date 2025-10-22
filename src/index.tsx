322b654daf7bcd34f5354157328c985e9808b4bf
Kirill Neruchev <kirill@neruchev.com>
2025-10-22 14:03:37 +0200

    Add JSX support

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-type ParamValues = Record<string, string | number | undefined>
+type ParamValues = Record<string, string | number | undefined | JSX.Element>
 
 type Options<T> = {
   variables?: ParamValues
@@ -20,7 +20,7 @@ export function hasI18nKey<T>(content: T, options: Options<T>): boolean {
   return !!content[key as string]
 }
 
-export function processI18N<T>(content: T, options: Options<T>): string {
+export function processI18N<T>(content: T, options: Options<T>): string | JSX.Element[] {
   const { variables, key } = options
   let response = content[key as string]
 
@@ -28,25 +28,61 @@ export function processI18N<T>(content: T, options: Options<T>): string {
     return response
   }
 
+  const { textVariables, jsxVariables } = Object.entries(variables).reduce<{
+    textVariables: Record<string, string>
+    jsxVariables: { index: number; key: string; value: JSX.Element }[]
+  }>(
+    (acc, [key, variable]) => {
+      if (typeof variable === 'object') {
+        acc.jsxVariables.push({
+          index: response.indexOf(key),
+          value: variable,
+          key,
+        })
+      } else {
+        acc.textVariables[key] = String(variable)
+      }
+
+      return acc
+    },
+    { textVariables: {}, jsxVariables: [] },
+  )
+
   // replace variables
-  for (const variable of Object.keys(variables)) {
+  for (const variable of Object.keys(textVariables)) {
     const reg = new RegExp(`{{${variable}}}`, 'g')
-    response = response.replace(reg, String(variables[variable]))
+    response = response.replace(reg, textVariables[variable])
   }
 
   // for declOfNum
   const testMatch = response.match(/(\[.+])/) // find [number|item1|item2|item3]
+
   if (testMatch?.[0]) {
     const replaceString = testMatch[0]
     const parseExpression = replaceString.match(/\[(.+)]/) // find number|item1|item2|item3
 
     if (parseExpression?.[1] && parseExpression[1].indexOf('|') > -1) {
       const [counter, ...strings] = parseExpression[1].split('|') // to number, item1, item2, item3
+
       if (strings?.length > 0) {
         const counterData = Number(counter)
         response = response.replace(replaceString, declOfNum(counterData, strings))
       }
     }
+  }
+
+  const orderedJSX = jsxVariables.sort((a, b) => a.index - b.index)
+
+  if (orderedJSX.length) {
+    const joinedKeys = orderedJSX.map((item) => `{{${item.key}}}`).join('|')
+    const parts = response.split(new RegExp(joinedKeys, 'g'))
+
+    return orderedJSX.reduce<JSX.Element[]>((acc, variable, index) => {
+      acc.push(createElement(Fragment, {}, parts[index]))
+      acc.push(variable.value)
+
+      return acc
+    }, [])
   }
 
   return response
